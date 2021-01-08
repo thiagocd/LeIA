61e5638c229a268ce5d416d9a0cf427a4f9c8bc4
Paula Guatura <paula.guatura@dextra-sw.com>
2021-01-07 21:42:13 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='leia_vader',
+    version='2.0.0',
+    description='Análise de Sentimentos em Português',
+    long_description="LeIA (Léxico para Inferência Adaptada) é um fork do léxico e ferramenta para análise de sentimentos VADER (Valence Aware Dictionary and sEntiment Reasoner) adaptado para textos em português, com suporte para emojis e foco na análise de sentimentos de textos expressos em mídias sociais - mas funcional para textos de outros domínios.",
+    url='https://github.com/pguatura/LeIA/',
+    author='rafjaa',
+    license='MIT',
+    classifiers=[
+        'Development Status :: Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=[
+    ],
+    packages=find_packages(),
+)
